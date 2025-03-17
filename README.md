186c26c88d95d1aebb10b8545754827cfea6f394
Diran <100717659+Oladiran-Dani3l@users.noreply.github.com>
2025-03-17 10:51:02 +0100

    Simple console app demonstrating understanding of scope and closures

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Week-9-YDKJS-study
 Simple console app built to demonstrate undestanding of concepts learned from YDKJS: Scopes &amp; Closures
+
+This simple counter app uses instances of scope (global, lexical, function and block) as well as hoisting and closure.
--- script.js
+++ script.js
@@ -1,1 +1,39 @@
+counter(); //Hoisting of function
 
+var newApp = "Counter App"; // Hoisted with undefined initially
+console.log("Welcome to", newApp);
+ 
+
+
+function counter() {
+    let count = 0; // count is a private variable of function  counter
+
+    console.log("Starting Counter...");
+
+    function increment() {
+        count++; // Increment inherits count from outer function, counter
+        console.log("Count:", count);
+
+        if (count % 3 === 0) {
+            let message = `Ternary count update! Count is now ${count}`;
+            console.log(message); // Block-scoped variable
+        }
+    }
+
+    function decrement() {
+        if (count > 0) {
+            count--;
+            console.log("Count:", count);
+        } else {
+            console.log("Count cannot go below zero!");
+        }
+    }
+
+    for (let i = 0; i < 10; i++) {
+        increment();
+    }
+
+    decrement();
+    decrement();
+    increment();
+}
